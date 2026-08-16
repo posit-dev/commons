eb6c97f5f2f6f5e931f228de370c80b1b0a33c1e
skaltman <sara.altman@posit.co>
2026-08-15 17:52:43 -0700

    Export rich_table function and add tests for as_measure_rich_table

--- tests/testthat/test-rich-table.R
+++ tests/testthat/test-rich-table.R
@@ -27,3 +27,24 @@ test_that("rich_table requires model-facing data to be a data frame", {
     error = TRUE
   )
 })
+
+test_that("as_measure_rich_table recognizes gt tables by class", {
+  value <- structure(list(name = "custom"), class = "gt_tbl")
+  data <- data.frame(term = "Headache", count = 7)
+  html <- "<table><tr><td>Headache</td><td>7</td></tr></table>"
+  local_mocked_bindings(
+    rich_table_data = function(...) data,
+    rich_table_html = function(...) html
+  )
+
+  result <- as_measure_rich_table(value)
+
+  expect_s3_class(result, "commons_rich_table")
+  expect_identical(result$value, value)
+  expect_identical(result$data, data)
+  expect_identical(result$html, html)
+})
+
+test_that("as_measure_rich_table ignores other values", {
+  expect_null(as_measure_rich_table(data.frame(x = 1)))
+})
