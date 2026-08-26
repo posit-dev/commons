6e445605c8f045d9dbfc8ede4e5b72d85426c271
Simon P. Couch <simonpatrickcouch@gmail.com>
2026-08-26 07:58:57 -0700

    cap run_r worker memory (#170)

--- pkg-r/R/run-r.R
+++ pkg-r/R/run-r.R
@@ -927,7 +927,8 @@ worker_init <- function(
     sym,
     read_roots,
     write_roots,
-    NULL,
+    # 8 GiB leaves ample headroom for light R; a lower Connect limit still applies.
+    8 * 1024^3,
     sandbox_mode,
     preserve_fds,
     network
